fb86166f62aaabce37eb60c9c36f9fd7fcafb804
Ridha Fahmi J <61994966+EvolutionRF@users.noreply.github.com>
2022-11-26 19:30:23 +0800

    update

--- app/Helpers/helpersFilesGuruInovatif.php
+++ app/Helpers/helpersFilesGuruInovatif.php
@@ -19,10 +19,8 @@ if (!function_exists('hello')) {
 if (!function_exists('checkAccessEnterFolder')) {
     function checkAccessEnterFolder($slug)
     {
-
         $folder = BaseFolders::where('slug', $slug)->first();
         $basefolder = "";
-
         if (!$folder) {
             $folder = Content::where('slug', $slug)->first();
             $basefolder = BaseFolders::find($folder->basefolder_id);
@@ -37,25 +35,25 @@ if (!function_exists('checkAccessEnterFolder')) {
             if ($folder->access_type == "public") {
                 return true;
             } else {
-                if ($basefolder != "") {
-                    if (auth()->user()->id == $folder->baseFolder->owner_id) {
-                        return true;
-                    }
-                    if (auth()->user()->id == $folder->owner_id) {
-                        return true;
-                    }
+                if (auth()->user()->id == $folder->owner_id) {
+                    return true;
                 } else {
+                    if ($basefolder != "null") {
+                        if ($folder->baseFolder->owner_id == auth()->user()->id) {
+                            return true;
+                        }
+                    }
+
                     if ($access_folder) {
-                        foreach ($folder->accesses as $access) {
+                        foreach ($access_folder as $access) {
                             if (auth()->user()->id == $access->user_id && $access->status == 'accept') {
                                 return true;
-                            } else {
-                                return false;
                             }
                         }
                     }
                 }
             }
+            return false;
         }
     }
 }
